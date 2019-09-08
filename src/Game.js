a0370b959a58bf58d7448731de09231cfdf3a4cf
aslanator <aslanator@mail.ru>
2019-09-08 22:34:37 +0300

    first steps

---
+++ src/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Field from './Game/Field/Field';
+import './Game.scss';
+
+function Game() {
+  return (
+    <div className="Game">
+      <Field/>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/Game/Field/Field.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './style.scss';
+
+class Field extends React.Component {
+
+
+    render () {
+
+        return (
+          <div className="field">
+              Field
+          </div>
+        )
+    }
+}
+
+export default Field;
